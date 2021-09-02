f65288969eee570e04a1d9f33bc65edea44b14c7
Ben Allen <ben@benallen.dev>
2021-09-02 20:22:55 +0200

    Add Tent DLC taunt, map numpad to Fn + LCTRL + number

--- keyboards/xd68/keymaps/benallen-dev/keymap.c
+++ keyboards/xd68/keymaps/benallen-dev/keymap.c
@@ -1,10 +1,11 @@
 #include QMK_KEYBOARD_H
 
-#define _BL 0
-#define _FL 1
+#define _BL 0 // Base Layer
+#define _FL 1 // Function Layer
 
 enum custom_keycodes {
     U_LEGIT = SAFE_RANGE,
+    U_TENT,
     EM_POOP,
     EM_XD,
     EM_ROFL,
@@ -12,9 +13,101 @@ enum custom_keycodes {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
+
+        /* Mapping numpad to Fn + Right Ctrl + number */
+        /* There's probably a better way to do this but naive implementation first, optimise later if necessary */
+        case KC_F1:
+            if (get_mods() && MOD_BIT(KC_RCTL)) {
+                record->event.pressed ? register_code(KC_KP_1) : unregister_code(KC_KP_1);
+                return false;
+            } else {
+                return true;
+            }
+            break;
+        case KC_F2:
+            if (get_mods() && MOD_BIT(KC_RCTL)) {
+                record->event.pressed ? register_code(KC_KP_2) : unregister_code(KC_KP_2);
+                return false;
+            } else {
+                return true;
+            }
+            break;
+        case KC_F3:
+            if (get_mods() && MOD_BIT(KC_RCTL)) {
+                record->event.pressed ? register_code(KC_KP_3) : unregister_code(KC_KP_3);
+                return false;
+            } else {
+                return true;
+            }
+            break;
+        case KC_F4:
+            if (get_mods() && MOD_BIT(KC_RCTL)) {
+                record->event.pressed ? register_code(KC_KP_4) : unregister_code(KC_KP_4);
+                return false;
+            } else {
+                return true;
+            }
+            break;
+        case KC_F5:
+            if (get_mods() && MOD_BIT(KC_RCTL)) {
+                record->event.pressed ? register_code(KC_KP_5) : unregister_code(KC_KP_5);
+                return false;
+            } else {
+                return true;
+            }
+            break;
+        case KC_F6:
+            if (get_mods() && MOD_BIT(KC_RCTL)) {
+                record->event.pressed ? register_code(KC_KP_6) : unregister_code(KC_KP_6);
+                return false;
+            } else {
+                return true;
+            }
+            break;
+        case KC_F7:
+            if (get_mods() && MOD_BIT(KC_RCTL)) {
+                record->event.pressed ? register_code(KC_KP_7) : unregister_code(KC_KP_7);
+                return false;
+            } else {
+                return true;
+            }
+            break;
+        case KC_F8:
+            if (get_mods() && MOD_BIT(KC_RCTL)) {
+                record->event.pressed ? register_code(KC_KP_8) : unregister_code(KC_KP_8);
+                return false;
+            } else {
+                return true;
+            }
+            break;
+        case KC_F9:
+            if (get_mods() && MOD_BIT(KC_RCTL)) {
+                record->event.pressed ? register_code(KC_KP_9) : unregister_code(KC_KP_9);
+                return false;
+            } else {
+                return true;
+            }
+            break;
+        case KC_F10:
+            if (get_mods() && MOD_BIT(KC_RCTL)) {
+                record->event.pressed ? register_code(KC_KP_0) : unregister_code(KC_KP_0);
+                return false;
+            } else {
+                return true;
+            }
+            break;
+
+        /* Pro Gamer macros */
+
         case U_LEGIT:
             if (record->event.pressed) SEND_STRING("\nseems legit \n");
             break;
+        case U_TENT:
+            if (record->event.pressed) SEND_STRING("\nI can't wait for the tent DLC so I can camp even harder \n");
+            break;
+
+        /* Emoji */
+
         case EM_POOP:
             if (record->event.pressed) send_unicode_string("💩");
             break;
@@ -52,22 +145,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /* Keymap _FL: Function Layer
      * ,----------------------------------------------------------------.
-     * |~ `| F1|F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|Del    |Ins |
+     * |~ `| F1|F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|Del    |Mute|
      * |----------------------------------------------------------------|
-     * |RESET|   |Up |   |   |   |   |   |   |   |   |   | 💩|     |Hax |
+     * |RESET|   |Up |   |   |   |   |   |   |   |💩 |   |   |Tent |Hax |
      * |----------------------------------------------------------------|
-     * |      |<- |Dn | ->|   |   |   |   |   |   |   |   |        |    |
+     * |      |<- |Dn | ->|   |   |   |   |   |   |   |   |        | M< |
      * |----------------------------------------------------------------|
-     * |UMODE   |   | 😂| 🤣|   |   |   |   |   |   |   |Play  | V+|Mute|
+     * |UMODE   |   | 😂| 🤣|   |   |   |   |   |   |   |Play  | V+| M> |
      * |----------------------------------------------------------------|
      * |    |    |    |       _______         |   |   |    |Hom|V- |End |
      * `----------------------------------------------------------------'
      */
     [_FL] = LAYOUT_65_ansi(
-        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_INS,
-        RESET,   _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, EM_POOP, _______, _______, _______, U_LEGIT,
-        _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
-        UC_MOD ,          _______, EM_XD  , EM_ROFL, _______, _______, _______, _______, _______, _______, _______, KC_MPLY, KC_VOLU, KC_MUTE,
+        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_MUTE,
+        RESET,   _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, EM_POOP, _______, _______, U_TENT,  U_LEGIT,
+        _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______, _______,          _______, KC_MPRV,
+        UC_MOD ,          _______, EM_XD  , EM_ROFL, _______, _______, _______, _______, _______, _______, _______, KC_MPLY, KC_VOLU, KC_MNXT,
         _______, _______, _______,                            _______,                   _______, _______, KC_TRNS, KC_HOME, KC_VOLD, KC_END
     ),
 };
