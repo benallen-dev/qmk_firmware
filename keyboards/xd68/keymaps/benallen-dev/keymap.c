6e8501582da20f48b4ddc406fe3126933e39bcaa
Ben Allen <ben@benallen.dev>
2021-11-15 07:35:01 +0100

    Add Alt+F10 and Alt+F4

--- keyboards/xd68/keymaps/benallen-dev/keymap.c
+++ keyboards/xd68/keymaps/benallen-dev/keymap.c
@@ -2,6 +2,7 @@
 
 #define _BL 0 // Base Layer
 #define _FL 1 // Function Layer
+#define _AL 2 // Alt Layer
 
 enum custom_keycodes {
     U_LEGIT = SAFE_RANGE,
@@ -9,6 +10,8 @@ enum custom_keycodes {
     EM_POOP,
     EM_XD,
     EM_ROFL,
+    ALT_F4,
+    ALT_F10,
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -17,7 +20,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         /* Mapping numpad to Fn + Right Ctrl + number */
         /* There's probably a better way to do this but naive implementation first, optimise later if necessary */
         case KC_F1:
-            if (get_mods() && MOD_BIT(KC_RCTL)) {
+            if (get_mods() & (MOD_BIT(KC_RCTL) == MOD_BIT(KC_RCTL))) {
                 record->event.pressed ? register_code(KC_KP_1) : unregister_code(KC_KP_1);
                 return false;
             } else {
@@ -25,7 +28,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
             break;
         case KC_F2:
-            if (get_mods() && MOD_BIT(KC_RCTL)) {
+            if (get_mods() & (MOD_BIT(KC_RCTL) == MOD_BIT(KC_RCTL))) {
                 record->event.pressed ? register_code(KC_KP_2) : unregister_code(KC_KP_2);
                 return false;
             } else {
@@ -33,7 +36,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
             break;
         case KC_F3:
-            if (get_mods() && MOD_BIT(KC_RCTL)) {
+            if (get_mods() & (MOD_BIT(KC_RCTL) == MOD_BIT(KC_RCTL))) {
                 record->event.pressed ? register_code(KC_KP_3) : unregister_code(KC_KP_3);
                 return false;
             } else {
@@ -41,7 +44,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
             break;
         case KC_F4:
-            if (get_mods() && MOD_BIT(KC_RCTL)) {
+            if (get_mods() & (MOD_BIT(KC_RCTL) == MOD_BIT(KC_RCTL))) {
                 record->event.pressed ? register_code(KC_KP_4) : unregister_code(KC_KP_4);
                 return false;
             } else {
@@ -49,7 +52,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
             break;
         case KC_F5:
-            if (get_mods() && MOD_BIT(KC_RCTL)) {
+            if (get_mods() & (MOD_BIT(KC_RCTL) == MOD_BIT(KC_RCTL))) {
                 record->event.pressed ? register_code(KC_KP_5) : unregister_code(KC_KP_5);
                 return false;
             } else {
@@ -57,7 +60,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
             break;
         case KC_F6:
-            if (get_mods() && MOD_BIT(KC_RCTL)) {
+            if (get_mods() & (MOD_BIT(KC_RCTL) == MOD_BIT(KC_RCTL))) {
                 record->event.pressed ? register_code(KC_KP_6) : unregister_code(KC_KP_6);
                 return false;
             } else {
@@ -65,7 +68,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
             break;
         case KC_F7:
-            if (get_mods() && MOD_BIT(KC_RCTL)) {
+            if (get_mods() & (MOD_BIT(KC_RCTL) == MOD_BIT(KC_RCTL))) {
                 record->event.pressed ? register_code(KC_KP_7) : unregister_code(KC_KP_7);
                 return false;
             } else {
@@ -73,7 +76,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
             break;
         case KC_F8:
-            if (get_mods() && MOD_BIT(KC_RCTL)) {
+            if (get_mods() & (MOD_BIT(KC_RCTL) == MOD_BIT(KC_RCTL))) {
                 record->event.pressed ? register_code(KC_KP_8) : unregister_code(KC_KP_8);
                 return false;
             } else {
@@ -81,7 +84,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
             break;
         case KC_F9:
-            if (get_mods() && MOD_BIT(KC_RCTL)) {
+            if (get_mods() & (MOD_BIT(KC_RCTL) == MOD_BIT(KC_RCTL))) {
                 record->event.pressed ? register_code(KC_KP_9) : unregister_code(KC_KP_9);
                 return false;
             } else {
@@ -89,7 +92,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
             break;
         case KC_F10:
-            if (get_mods() && MOD_BIT(KC_RCTL)) {
+            if (get_mods() & (MOD_BIT(KC_RCTL) == MOD_BIT(KC_RCTL))) {
                 record->event.pressed ? register_code(KC_KP_0) : unregister_code(KC_KP_0);
                 return false;
             } else {
@@ -117,6 +120,25 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case EM_ROFL:
             if (record->event.pressed) send_unicode_string("🤣");
             break;
+
+        /* Alt-layer keys */
+        case ALT_F4:
+            if (record->event.pressed) {
+                register_code(KC_LALT);
+                register_code(KC_F4);
+            } else {
+                unregister_code(KC_F4);
+                unregister_code(KC_LALT);            }
+            break;
+        case ALT_F10:
+            if (record->event.pressed) {
+                register_code(KC_LALT);
+                register_code(KC_F10);
+            } else {
+                unregister_code(KC_F10);
+                unregister_code(KC_LALT);
+            }
+            break;
     }
     return true;
 };
@@ -136,7 +158,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `----------------------------------------------------------------'
      */
     [_BL] = LAYOUT_65_ansi(
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_F6,
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_MUTE,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_F3,
         KC_GRV,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN,
@@ -157,10 +179,40 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `----------------------------------------------------------------'
      */
     [_FL] = LAYOUT_65_ansi(
-        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_MUTE,
+        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_F6,
         RESET,   _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, EM_POOP, _______, _______, U_TENT,  U_LEGIT,
         _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______, _______,          _______, KC_MPRV,
         UC_MOD ,          _______, EM_XD  , EM_ROFL, _______, _______, _______, _______, _______, _______, _______, KC_MPLY, KC_VOLU, KC_MNXT,
         _______, _______, _______,                            _______,                   _______, _______, KC_TRNS, KC_HOME, KC_VOLD, KC_END
     ),
+
+    /* Keymap _AL: Alt Layer
+     * ,----------------------------------------------------------------.
+     * |~ `| F1|F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|Del    |Mute|
+     * |----------------------------------------------------------------|
+     * |RESET|   |Up |   |   |   |   |   |   |   |💩 |   |   |Tent |Hax |
+     * |----------------------------------------------------------------|
+     * |      |<- |Dn | ->|   |   |   |   |   |   |   |   |        | M< |
+     * |----------------------------------------------------------------|
+     * |UMODE   |   | 😂| 🤣|   |   |   |   |   |   |   |Play  | V+| M> |
+     * |----------------------------------------------------------------|
+     * |    |    |    |       _______         |   |   |    |Hom|V- |End |
+     * `----------------------------------------------------------------'
+     */
+    [_AL] = LAYOUT_65_ansi(
+        _______, _______, _______, _______,  ALT_F4, _______, _______, _______, _______, _______, ALT_F10, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______,                            _______,                   _______, _______, _______, _______, _______, _______
+    ),
 };
+
+
+// [_LAYER] = LAYOUT_65_ansi(
+//         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+//         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+//         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
+//         _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+//         _______, _______, _______,                            _______,                   _______, _______, _______, _______, _______, _______
+//     ),
